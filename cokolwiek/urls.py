6aa92496f7211343c0e0c79ba10878a44ed06f44
Ofey <mat.janczura@gmail.com>
2020-12-05 20:57:10 +0100

    dodanie templatek cennika oraz formularza

--- cokolwiek/urls.py
+++ cokolwiek/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index),
+    path('cennik', views.price_list),
+    path('formularz_kontaktowy', views.form_contact),
 ]
--- cokolwiek/views.py
+++ cokolwiek/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'strona_przedszkole/index.html')
+
+
+def price_list(request):
+    return render(request, 'strona_przedszkole/price_list.html')
+
+def form_contact(request):
+    return render(request, 'strona_przedszkole/form_contact.html')
