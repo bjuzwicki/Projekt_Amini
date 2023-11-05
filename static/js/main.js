1264c787eb6a87beac0f0b443f033d6ff22f1a2a
bjuzwicki <bartlomiej.juzwicki@wp.pl>
2023-11-05 23:54:41 +0100

    Mati widać po mnie?

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,5 @@
 document.addEventListener('DOMContentLoaded', function ()
 {
-
-    // dodaje cień do nawigacji
-    // const nav = document.querySelector('.navbar')
-
-    // function addShadow() {
-    //     if (window.scrollY >= 200) {
-    //         nav.classList.add('shadow-bg')
-    //     } else {
-    //         nav.classList.remove('shadow-bg')
-    //     }
-    // }
-    // window.addEventListener('scroll', addShadow)
-
-
     // skrypt naprawiający nawigację (zamyka się po kliknięciu w cokolwiek na mobilce)
     $(document).click(function (event) {
         var clickover = $(event.target);
@@ -23,27 +9,88 @@ document.addEventListener('DOMContentLoaded', function ()
         }
     });
 
-    // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
+
+// Funkcja sprawdzająca, czy plik cookie z zgodą istnieje
+function checkCookie() {
+    const cookiesAccepted = getCookie("aminiCookies");
+    if (cookiesAccepted !== "true") {
+        cookieContainer.style.display = "flex";
+    }
+}
+
+// Funkcja do pobierania wartości pliku cookie o podanej nazwie
+function getCookie(cookieName) {
+    const name = cookieName + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+// Obsługa przycisku "Akceptuj" cookie
+const cookieContainer = document.getElementById("cookieContainer");
+const cookieAccept = document.getElementById("cookieAccept");
+
+cookieAccept.addEventListener("click", () => {
+    cookieContainer.style.display = "none";
+
+    // Ustawienie pliku cookie na 30 dni
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 30);
+    document.cookie = "aminiCookies=true; expires=" + expirationDate.toUTCString();
+});
+
+// Sprawdź, czy plik cookie został już zaakceptowany po załadowaniu strony
+window.addEventListener("load", checkCookie);
 
 function scrollFunction()
 {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
-  {
-    document.getElementById("navbar").style.transition = "padding 0.8s";
-    document.getElementById("navbar").style.padding = "5px 80px";
-    document.getElementById("logo").style.transition = "width 0.8s, height 0.8s";
-    document.getElementById("logo").style.width = "250px";
-    document.getElementById("logo").style.height = "80px";
-  }
-  else
-  {
-    document.getElementById("navbar").style.transition = "padding 0.8s";
-    document.getElementById("navbar").style.padding = "5px 80px";
-    document.getElementById("logo").style.transition = "width 0.8s, height 0.8s";
-    document.getElementById("logo").style.width = "300px";
-    document.getElementById("logo").style.height = "100px";
-  }
+    var screenWidth = window.screen.width;
+    var screenHeight = window.screen.height;
+
+    if(window.screen.width > 1000)
+    {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
+        {
+            document.getElementById("navbar").style.transition = "padding 0.8s";
+            document.getElementById("navbar").style.padding = "5px 80px";
+            document.getElementById("logo").style.transition = "width 0.8s, height 0.8s";
+            document.getElementById("logo").style.width = "260px";
+            document.getElementById("logo").style.height = "80px";
+        }
+        else
+        {
+            document.getElementById("navbar").style.transition = "padding 0.8s";
+            document.getElementById("navbar").style.padding = "5px 80px";
+            document.getElementById("logo").style.transition = "width 0.8s, height 0.8s";
+            document.getElementById("logo").style.width = "300px";
+            document.getElementById("logo").style.height = "100px";
+        }
+    }
+    else
+    {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
+        {
+            document.getElementById("data").style.display = "none";
+            document.getElementById("brand").style.width = "70%";
+        }
+        else
+        {
+            document.getElementById("data").style.display = "block";
+            document.getElementById("brand").style.width = "80%";
+        }
+    }
+
 }
 
 
@@ -102,3 +149,8 @@ function scrollFunction()
         ]
     });
 });
+
+// Funkcja do wyświetlania pełnego zdjęcia w overlay
+function showImage(imageUrl, altText) {
+        window.open(imageUrl, '_blank');
+}
